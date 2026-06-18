9841d273caf59e2235504aaac8956ca01c5a9d62
surajchouhan1845 <gm.surajchouhan1845@gmail.com>
2026-06-18 20:53:31 +0530

    update code 12

---
+++ backend/controllers/dashboardController.js
@@ -0,0 +1,14 @@
+const { json } = require("express");
+const User = require("../models/User");
+
+const dashboardInfo = async (req,res)=>{
+    try{
+        const user = await User.findOne(req.user.id).select("-password");
+        res.json({message:"Welcome to Dashboard",user});
+    }catch(error)
+    {
+        res.status(500).json({message:"server error"});
+    }
+}
+
+module.exports = { dashboardInfo };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,17 +4,11 @@ const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 const { registerUser, loginUser } = require("../controllers/authController");
+const { dashboardInfo } = require("../controllers/dashboardController");
 
 router.post("/register", registerUser);
 router.post("/login",loginUser);
 
-router.get("/dashboard",authMiddleware,
-    (req,res)=>{
-        res.json({
-            message:"Welcome to Dashboard",
-            user:res.user
-        });
-    }
-);
+router.get("/dashboard",authMiddleware,dashboardInfo);
 
 module.exports = router;
